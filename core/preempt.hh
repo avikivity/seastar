a8ce17d4b147895ad1e405c6236f6396b9be9a25
Avi Kivity <avi@scylladb.com>
2017-01-31 13:31:51 +0200

    wip

--- core/preempt.hh
+++ core/preempt.hh
@@ -39,6 +39,8 @@ inline bool need_preempt() {
     std::atomic_signal_fence(std::memory_order_seq_cst);
     return g_need_preempt().load(std::memory_order_relaxed) <= 0;
 #else
+    // Make scheduler think we exhausted all our ticks
+    g_need_preempt().store(0, std::memory_order_relaxed);
     return true;
 #endif
 }
